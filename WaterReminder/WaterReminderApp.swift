febd1cf7171e0b484da2a2a2d34a6b634cc9d661
xx cheng <1042841074@qq.com>
2025-02-24 22:27:34 +0800

    Initial Commit

---
+++ WaterReminder/WaterReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterReminderApp.swift
+//  WaterReminder
+//
+//  Created by 程星涵 on 24/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct WaterReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaterReminderTests/WaterReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterReminderTests.swift
+//  WaterReminderTests
+//
+//  Created by 程星涵 on 24/2/2025.
+//
+
+import Testing
+@testable import WaterReminder
+
+struct WaterReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
